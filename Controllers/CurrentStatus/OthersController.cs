d18652ddddf7e420da2d8f1e7df843b8041a6a99
shukladhaval25 <shukla_dhaval@yahoo.com>
2019-07-25 00:05:44 +0530

    Add Others controller for other investment in current status.

---
+++ Controllers/CurrentStatus/OthersController.cs
@@ -0,0 +1,91 @@
+﻿using FinancialPlanner.Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using FinancialPlanner.Common.Model.CurrentStatus;
+using FinancialPlanner.BusinessLogic.CurrentStatus;
+
+namespace FinancialPlanner.Controllers.CurrentStatus
+{
+    public class OthersController : ApiController
+    {
+        [Route("api/Others/GetAll")]
+        [HttpGet]
+        public Result<IList<Others>> GetAll(int plannerId)
+        {
+            var result = new Result<IList<Others>>();
+            OthersService othersSrvice = new OthersService();
+            result.Value = othersSrvice.GetAll(plannerId);
+            result.IsSuccess = true;
+            return result;
+        }
+
+        [Route("api/Others/Get")]
+        [HttpGet]
+        public Result<Others> Get(int id)
+        {
+            var result = new Result<Others>();
+            OthersService othersSrvice = new OthersService();
+            result.Value = othersSrvice.Get(id);
+            result.IsSuccess = true;
+            return result;
+        }
+
+        [Route("api/Others/Add")]
+        [HttpPost]
+        public Result Add(Others Others)
+        {
+            var result = new Result();
+            try
+            {
+                OthersService othersSrvice = new OthersService();
+                othersSrvice.Add(Others);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+
+        [Route("api/Others/Update")]
+        [HttpPost]
+        public Result Update(Others Others)
+        {
+            var result = new Result();
+            try
+            {
+                OthersService othersSrvice = new OthersService();
+                othersSrvice.Update(Others);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+
+        [Route("api/Others/Delete")]
+        [HttpPost]
+        public Result Delete(Others Others)
+        {
+            var result = new Result();
+            try
+            {
+                OthersService othersSrvice = new OthersService();
+                othersSrvice.Delete(Others);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+    }
+}
---
+++ Controllers/TaskManagement/TaskProjectController.cs
@@ -0,0 +1,67 @@
+﻿using FinancialPlanner.BusinessLogic.TaskManagements;
+using FinancialPlanner.Common.Model;
+using FinancialPlanner.Common.Model.TaskManagement;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace FinancialPlanner.Controllers.TaskManagement
+{
+    public class TaskProjectController : ApiController
+    {
+        [Route("api/TaskProjectController/GetAll")]
+        [HttpGet]
+        public Result<IList<Project>> GetAll()
+        {
+            var result = new Result<IList<Project>>();
+            TaskProjectService  taskProjectService = new TaskProjectService();
+
+            var lstProject = taskProjectService.GetAll();
+            result.Value = (IList<Project>)lstProject;
+            result.IsSuccess = true;
+            return result;
+        }
+
+        [Route("api/TaskProjectController/Get")]
+        [HttpGet]
+        public Result<Project> Get(int id)
+        {
+            var result = new Result<Project>();
+            TaskProjectService taskProjectService = new TaskProjectService();
+
+            result.Value = taskProjectService.GetById(id);
+            result.IsSuccess = true;
+            return result;
+        }
+
+        // POST: api/TaskProject
+        [Route("api/TaskProjectController/Add")]
+        [HttpPost]
+        public Result Add(Project project)
+        {
+            var result = new Result();
+            try
+            {
+                TaskProjectService projectService = new TaskProjectService();
+                projectService.Add(project);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+
+        // PUT: api/TaskProject/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TaskProject/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
