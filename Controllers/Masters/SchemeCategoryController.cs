6699f2e1c2e50452c814c7cda11ca76c5e23a139
shukladhaval25 <shukla_dhaval@yahoo.com>
2019-10-30 00:00:55 +0530

    Add scheme category controller.

---
+++ Controllers/Masters/SchemeCategoryController.cs
@@ -0,0 +1,66 @@
+﻿using FinancialPlanner.BusinessLogic.ApplicationMaster;
+using FinancialPlanner.Common.Model;
+using FinancialPlanner.Common.Model.Masters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinancialPlanner.Controllers.Masters
+{
+    public class SchemeCategoryController : ApiController
+    {
+        [Route("api/SchemeCategory/GetAll")]
+        [HttpGet]
+        public Result<IList<SchemeCategory>> GetAll()
+        {
+            var result = new Result<IList<SchemeCategory>>();
+            SchemeCategoryService SchemeCategoryService = new SchemeCategoryService();
+
+            var lstClient = SchemeCategoryService.Get();
+            result.Value = (IList<SchemeCategory>)lstClient;
+            result.IsSuccess = true;
+            return result;
+        }
+
+        [Route("api/SchemeCategory/Add")]
+        [HttpPost]
+        public Result Add(SchemeCategory schemeCategory)
+        {
+            var result = new Result();
+            try
+            {
+                SchemeCategoryService AreaService = new SchemeCategoryService();
+                AreaService.Add(schemeCategory);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+
+        [Route("api/SchemeCategory/Delete")]
+        [HttpDelete]
+        public Result Delete(SchemeCategory schemeCategory)
+        {
+            var result = new Result();
+            try
+            {
+                SchemeCategoryService schemeCategoryService = new SchemeCategoryService();
+                schemeCategoryService.Delete(schemeCategory);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+    }
+}
---
+++ Controllers/PlanOptions/InvestmentRecommendationController.cs
@@ -0,0 +1,83 @@
+﻿using FinancialPlanner.Common.Model;
+using FinancialPlanner.Common.Model.PlanOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinancialPlanner.Controllers.PlanOptions
+{
+    public class InvestmentRecommendationController : ApiController
+    {
+        [Route("api/InvestmentRecommendationController/Get")]
+        [HttpGet]
+        public Result<InvestmentRecommendation> Get(int plannerId)
+        {
+            Result<InvestmentRecommendation> result = new Result<InvestmentRecommendation>();
+            //CashFlowService cashFlowService = new CashFlowService();
+            //result.Value = cashFlowService.Get(plannerId);
+            result.IsSuccess = true;
+            return result;
+        }
+
+        [Route("api/InvestmentRecommendationController/Add")]
+        [HttpPost]
+        public Result Add(InvestmentRecommendation InvestmentRecommendation)
+        {
+
+            var result = new Result();
+            try
+            {
+                //CashFlowService cashFlowService = new CashFlowService();
+                //cashFlowService.Add(InvestmentRecommendation);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+
+        [Route("api/InvestmentRecommendationController/Update")]
+        [HttpPost]
+        public Result Update(InvestmentRecommendation InvestmentRecommendation)
+        {
+            var result = new Result();
+            try
+            {
+                //CashFlowService cashFlowService = new CashFlowService();
+                //cashFlowService.Update(InvestmentRecommendation);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+
+        [Route("api/InvestmentRecommendationController/Delete")]
+        [HttpPost]
+        public Result Delete(InvestmentRecommendation cashFlow)
+        {
+            var result = new Result();
+            try
+            {
+                //CashFlowService cashFlowService = new CashFlowService();
+                //cashFlowService.Delete(cashFlow);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+    }
+}
