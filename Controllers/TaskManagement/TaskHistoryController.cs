93a35b82f036222e555f433cfa993f1be9856a05
shukladhaval25 <shukla_dhaval@yahoo.com>
2019-11-28 08:40:25 +0530

    Add task history controller

---
+++ Controllers/TaskManagement/TaskHistoryController.cs
@@ -0,0 +1,28 @@
+﻿using FinancialPlanner.BusinessLogic.TaskManagements;
+using FinancialPlanner.Common.Model;
+using FinancialPlanner.Common.Model.TaskManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinancialPlanner.Controllers.TaskManagement
+{
+    public class TaskHistoryController : ApiController
+    {
+        [Route("api/TaskHistory/GetAll")]
+        [HttpGet]
+        public Result<IList<TaskHistory>> GetAll(int taskId)
+        {
+            var result = new Result<IList<TaskHistory>>();
+            TaskHistoryService taskHistoryService = new TaskHistoryService();
+
+            var taskHistories = taskHistoryService.GetAll(taskId);
+            result.Value = (IList<TaskHistory>)taskHistories;
+            result.IsSuccess = true;
+            return result;
+        }
+    }
+}
