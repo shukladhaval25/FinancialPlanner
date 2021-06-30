9f625d822326dfa90de7e9c0ec367fa9e6538dd9
shukla <shukl@DESKTOP-FT786DU>
2021-06-30 11:26:35 +0530

    Add score related controller

---
+++ Controllers/ScoreCalculation/ScoreController.cs
@@ -0,0 +1,82 @@
+﻿using FinancialPlanner.BusinessLogic.ScoreCalculation;
+using FinancialPlanner.Common.Model;
+using FinancialPlanner.Common.Model.ScoreCalculation;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace FinancialPlanner.Controllers.ScoreCalculation
+{
+    public class ScoreController : ApiController
+    {
+        [Route("api/Score")]
+        [HttpGet]
+        public Result<IList<Score>> GetAll()
+        {
+            var result = new Result<IList<Score>>();
+            ScoreService ScoreService = new ScoreService();
+
+            var lstClient = ScoreService.Get();
+            result.Value = (IList<Score>)lstClient;
+            result.IsSuccess = true;
+            return result;
+        }
+
+        [Route("api/Score")]
+        [HttpPost]
+        public Result Add(List<Score> Scores)
+        {
+            var result = new Result();
+            try
+            {
+                ScoreService ScoreService = new ScoreService();
+                ScoreService.Add(Scores);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+
+        //[Route("api/Score")]
+        //[HttpPost]
+        //public Result Update(Score Score)
+        //{
+        //    var result = new Result();
+        //    try
+        //    {
+        //        ScoreService ScoreService = new ScoreService();
+        //        ScoreService.Update(Score);
+        //        result.IsSuccess = true;
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        result.IsSuccess = false;
+        //        result.ExceptionInfo = exception;
+        //    }
+        //    return result;
+        //}
+
+        [Route("api/Score")]
+        [HttpDelete]
+        public Result Delete(Score Score)
+        {
+            var result = new Result();
+            try
+            {
+                ScoreService ScoreService = new ScoreService();
+                ScoreService.Delete(Score);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+    }
+}
---
+++ Controllers/ScoreCalculation/ScoreEntryController.cs
@@ -0,0 +1,95 @@
+﻿using FinancialPlanner.Common.Model;
+using FinancialPlanner.Common.Model.ScoreCalculation;
+using FinancialPlanner.BusinessLogic.ScoreCalculation;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace FinancialPlanner.Controllers.ScoreEntryCalculation
+{
+    public class ScoreEntryEntryController : ApiController
+    {
+        [Route("api/ScoreEntry")]
+        [HttpGet]
+        public Result<IList<ScoreEntry>> GetAll()
+        {
+            var result = new Result<IList<ScoreEntry>>();
+            ScoreEntryService ScoreEntryService = new ScoreEntryService();
+
+            var lstClient = ScoreEntryService.Get();
+            result.Value = (IList<ScoreEntry>)lstClient;
+            result.IsSuccess = true;
+            return result;
+        }
+
+        [Route("api/ScoreEntry/{0}")]
+        [HttpGet]
+        public Result<IList<ScoreEntry>> GetAll(DateTime entryDate)
+        {
+            var result = new Result<IList<ScoreEntry>>();
+            ScoreEntryService ScoreEntryService = new ScoreEntryService();
+
+            var lstClient = ScoreEntryService.Get(entryDate);
+            result.Value = (IList<ScoreEntry>)lstClient;
+            result.IsSuccess = true;
+            return result;
+        }
+
+        [Route("api/ScoreEntry")]
+        [HttpPost]
+        public Result Add(List<ScoreEntry> ScoreEntrys)
+        {
+            var result = new Result();
+            try
+            {
+                ScoreEntryService ScoreEntryService = new ScoreEntryService();
+                ScoreEntryService.Add(ScoreEntrys);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+
+        [Route("api/ScoreEntry")]
+        [HttpPut]
+        public Result Update(List<ScoreEntry> ScoreEntrys)
+        {
+            var result = new Result();
+            try
+            {
+                ScoreEntryService ScoreEntryService = new ScoreEntryService();
+                ScoreEntryService.Update(ScoreEntrys);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+
+        [Route("api/ScoreEntry")]
+        [HttpDelete]
+        public Result Delete(ScoreEntry ScoreEntry)
+        {
+            var result = new Result();
+            try
+            {
+                ScoreEntryService ScoreEntryService = new ScoreEntryService();
+                ScoreEntryService.Delete(ScoreEntry);
+                result.IsSuccess = true;
+            }
+            catch (Exception exception)
+            {
+                result.IsSuccess = false;
+                result.ExceptionInfo = exception;
+            }
+            return result;
+        }
+    }
+}
